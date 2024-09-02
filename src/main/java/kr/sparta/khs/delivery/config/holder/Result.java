96ec2dc399f8465abd8a70b0e13b32ddd8df8af3
Minsu Jo <tok0419@naver.com>
2024-09-02 22:54:46 +0900

    SETTING : COMMON RESPONSE OBJECT DESCRIPTION

--- src/main/java/kr/sparta/khs/delivery/config/holder/Result.java
+++ src/main/java/kr/sparta/khs/delivery/config/holder/Result.java
@@ -1,6 +1,7 @@
 package kr.sparta.khs.delivery.config.holder;
 
 import com.sun.net.httpserver.Authenticator;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,13 +11,19 @@ import lombok.experimental.FieldDefaults;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Schema(title = "공통 응답 객체")
 public class Result<T> {
 
+    @Schema(hidden = true)
     private static final String SUCCESS = "SUCCESS";
+    @Schema(hidden = true)
     private static final String FAILURE = "FAILURE";
 
+    @Schema(description = "응답 결과", example = "SUCCESS")
     private String result;
+    @Schema(description = "응답 데이터")
     private T data;
+    @Schema(description = "(에러)메세지", example = "일치하는 요소가 없습니다.")
     private String message;
 
     public static <T> Result<T> success(T data) {
